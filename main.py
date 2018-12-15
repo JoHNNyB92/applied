604b8c55b33fb6e0765de5fe1790425b1adddab4
root <j.avgeros1992@hotmail.gr>
2018-12-15 12:50:39 +0200

    made noise work and fixed csv

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-def generate_noise(min_max,length,keys,df,enable_plot=False):
+def generate_noise(min_max,keys,length,df,enable_plot=False):
     noise_array={}
     records={}
     for key in keys:
         mean=round((float)(min_max[key][0]-min_max[key][1])/2,1)
         std=round((float)(min_max[key][0]-min_max[key][1])/6,1)
+        print length
         noise_array[key]=np.random.normal(mean, std, length)
         records[key]=np.random.uniform(low=0.0, high=1.0, size=length)
         if enable_plot==True:
@@ -17,22 +18,41 @@ def generate_noise(min_max,length,keys,df,enable_plot=False):
             plt.show()
             plt.gcf().clear()
             #noise_array.append(np.random.normal(, , length))
+    return (noise_array,records)
             
         
-#def add_noise():
-#    #TODO
+def add_noise(noisy,noisy_probs,dfs,feat_prob):
+    cnt=0
+    for key in dfs.keys():
+        if len(dfs[keys])!=cnt:
+            for rec in range(dfs[key].count()):
+                prob=noisy_probs[key][rec]
+                if prob<feat_prob[key]:
+                    print 'Adding noise to '+key+',record '+str(rec)+' ,previously value '+str(dfs[key][rec])+', noise '+str(noisy[key][rec])+',new value '+str(dfs[key][rec]+noisy[key][rec])
+                    dfs[key][rec]=dfs[key][rec]+noisy[key][rec]
+        cnt=cnt+1
+    return dfs
 
+    
+def write_excel_pd(pd_noise_array):
+    pathname='noisy.xlsx'
+    writer = pd.ExcelWriter(pathname)
+    pd_noise_array.to_excel(writer,'Sheet1')
+    writer.save()
 
 
 if __name__ == "__main__":
-    dfs = pd.read_excel("players.xlsx")
-    length=df['FTA'].count()
-    noise_prop = [0.1 for _ in range(length)]
-    noise_prop_dic={}
-    print dfs.keys()
+    dfs = pd.read_excel("players_stats.xlsx")
     df = pd.DataFrame(dfs, columns=dfs.keys())
+    noise_prop = [0.1 for _ in range(len(dfs.keys()))]
     min_max={}
-    df=df.drop('Player', axis=1)
+    noise_prop_dict={}
+    
+    
+    for key in df.keys():
+        print 'Filling empty values with 0 for feature '+key
+        df[key].fillna(0, inplace=True)
+    length=df[key].count()
     cnt=0
     for key in df.keys():
         noise_prop_dict[key]=noise_prop[cnt]
@@ -40,47 +60,14 @@ if __name__ == "__main__":
         min_max[key]=[df.loc[df[key].idxmax()][key],df.loc[df[key].idxmin()][key]]
         print min_max[key]
     
-    headers=df.keys()
+    headers=df.keys()  
+    noisy={}
+    noisy_probs={}
     if len(sys.argv)!=1:
-        generate_noise(noise_prop_dict,min_max,length,headers,dfs,True)
+        (noisy,noisy_probs)=generate_noise(min_max,headers,length,dfs,True)
     else:
-        generate_noise(noise_prop_dict,min_max,length,headers,dfs)
-    
-    '''miss = prop
-    outlier = prop
-    swap = prop
-    mixed = []
-    ms=0
-    sw=0
-    o=0
-    for i, v in enumerate(gen):
-        cols = len(v)
-        uni = np.random.uniform(0, 1, 3)
-        
-        if uni[0] < miss:
-            ms=ms+1
-            v[int(np.random.uniform(0, cols, 1)[0])] = 'NA'
-            #print('NA in record ', i)
- 
-        if uni[1] < swap:
-            sw=sw+1
-            swap_p = np.random.uniform(0, cols, 2)
-            first = int(swap_p[0])
-            second = int(swap_p[1])
-            temp = v[first]
-            v[first] = v[second]
-            v[second] = temp
-            #print('Swapping record ', i, ' columns ', first, '-', second)
- 
-        if uni[2] < outlier:
-            o=o+1
-            outlier_indices =[0,1,5,11,12,13]
-            n = int(np.random.uniform(0, len(outlier_indices), 1)[0])
-            #print("Outlier in record ", i, " with index ", n)
-            v = moutlier(v, outlier_indices[n])
- 
-        mixed.append(v)
-    print 'Added ',ms,' NA records, swapped ',sw,' fields in records and inserted outliers in ', o,' records.'
-    create_csv(mixed)'''
+        (noisy,noisy_probs)=generate_noise(min_max,headers,length,dfs)
+    dfs=add_noise(noisy,noisy_probs,df,noise_prop_dict)
+    write_excel_pd(dfs)
 	
  
